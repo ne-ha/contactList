6a7d5666912c5b2b2401cdbeefa5761ff9a9ae0c
NEHA SUWAl <neha@jyaasa.com>
2015-03-16 14:17:27 +0545

    Added routes for callback action in contacts controller

--- contactList/config/routes.rb
+++ contactList/config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  resources :contacts
-
+  resources :contacts do
+    get 'callback'
+  end
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
