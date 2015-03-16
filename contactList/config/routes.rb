43bc9310c6f8883edaffc741c3664c839c50b106
NEHA SUWAl <neha@jyaasa.com>
2015-03-16 14:26:10 +0545

    Removed resources and added get method for contacts's callback.

--- contactList/config/routes.rb
+++ contactList/config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  resources :contacts do
-    get 'callback'
-  end
+  get 'contacts/callback' => "contacts#callback"
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
