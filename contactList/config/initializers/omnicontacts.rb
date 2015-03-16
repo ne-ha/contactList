9fd660226b0c0962b23199640551b7336295ab35
NEHA SUWAl <neha@jyaasa.com>
2015-03-16 13:56:23 +0545

    Added client id and client secret for both gmail and facebook.

--- contactList/config/initializers/omnicontacts.rb
+++ contactList/config/initializers/omnicontacts.rb
@@ -1,6 +1,8 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "client_id", "client_secret", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
-  importer :facebook, "client_id", "client_secret"
+  importer :gmail, "
+    775333370063-engsqoo5192smsn7lm22fn0q8jn91chu.apps.googleusercontent.com", "
+    n-AqWAQirRECrT8RYjNGEHiu", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+  importer :facebook, "750035695112072", "91d7cd52089583689650cd2256052bca"
 end
