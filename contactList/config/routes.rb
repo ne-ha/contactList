6d58db7ad40de73d9ba384eeabaa50ff6341a2f4
NEHA SUWAl <neha@jyaasa.com>
2015-03-16 15:18:56 +0545

    Instead of provider wrote importer.

--- contactList/config/routes.rb
+++ contactList/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get "/contacts/:provider/callback" => "contacts#callback"
+  get "/contacts/:importer/callback" => "contacts#callback"
 
   get "/contacts/failure" => "contacts#failure"
   
