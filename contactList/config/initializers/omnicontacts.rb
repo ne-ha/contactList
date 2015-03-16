da34a8fab289b161df0c79c9cee00fd3e2b410e9
NEHA SUWAl <neha@jyaasa.com>
2015-03-16 14:30:04 +0545

    Updated the client id , client secret and the redirect path for gmail.

--- contactList/config/initializers/omnicontacts.rb
+++ contactList/config/initializers/omnicontacts.rb
@@ -1,8 +1,6 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "
-    775333370063-engsqoo5192smsn7lm22fn0q8jn91chu.apps.googleusercontent.com", "
-    n-AqWAQirRECrT8RYjNGEHiu", {:redirect_path => "/oauth2callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
+  importer :gmail, “775333370063-engsqoo5192smsn7lm22fn0q8jn91chu.apps.googleusercontent.com”, “n-AqWAQirRECrT8RYjNGEHiu”, {:redirect_path => “/contacts/callback”, :ssl_ca_file => “/etc/ssl/certs/curl-ca-bundle.crt”}
   importer :facebook, "750035695112072", "91d7cd52089583689650cd2256052bca"
 end
