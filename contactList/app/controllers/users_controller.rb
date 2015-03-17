2a53f5e0056d7604c8c292da83678da1b78b7d15
NEHA SUWAl <neha@jyaasa.com>
2015-03-17 12:56:36 +0545

    Added necesary codes to access the friend's list of loged in user

--- contactList/app/controllers/users_controller.rb
+++ contactList/app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   end
 
   def login
+    auth = request.env["omniauth.auth"]
+    fb_access_token = auth['credentials']['token']
     @loged_user = User.koala(request.env['omniauth.auth']['credentials'])
-    token = "CAACEdEose0cBAIv6iutkHnCNbkMLMYjCMLW0kks0FYeIXE6Hr0v2dzeJknDdCEZCc7rf1yNfuqCZBZCGCU7dKwJ2PRSa2NnP3cJQMuq9nOQSLCsJ5cxjS20ZAn4epTsm2SiP0vQkRrWQPw5wZBRVbZBMsPSnTe5ZA3ycuekceZCKngA6jrBlybY3ZCjYPqSF83R7O1okU7Mx1eLEI54ZBh8w9F"
-    @graph = Koala::Facebook::API.new(token)
-    @friends = @graph.get_connections("me", "friends?fields=id, name, picture.type(small)")
+    @graph = Koala::Facebook::API.new(fb_access_token)
+    @friends = @graph.get_connections("me", "friends?fields=id, name, picture.type(small)") 
   end
 end
