78110a8bddfd881bc077858f18d8dbf4d8a1add8
NEHA SUWAl <neha@jyaasa.com>
2015-03-17 10:08:04 +0545

    Removed @user.

--- contactList/app/controllers/users_controller.rb
+++ contactList/app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
     @loged_user = User.koala(request.env['omniauth.auth']['credentials'])
     token = "CAACEdEose0cBAIv6iutkHnCNbkMLMYjCMLW0kks0FYeIXE6Hr0v2dzeJknDdCEZCc7rf1yNfuqCZBZCGCU7dKwJ2PRSa2NnP3cJQMuq9nOQSLCsJ5cxjS20ZAn4epTsm2SiP0vQkRrWQPw5wZBRVbZBMsPSnTe5ZA3ycuekceZCKngA6jrBlybY3ZCjYPqSF83R7O1okU7Mx1eLEI54ZBh8w9F"
     @graph = Koala::Facebook::API.new(token)
-    @user = @graph.get_object("me")
     @friends = @graph.get_connections("me", "friends")
   end
 end
