e270e5eed3a980ba12b37eae267b33bc7057516f
NEHA SUWAl <neha@jyaasa.com>
2015-03-17 09:13:16 +0545

    Added codes to access the current user's friends's list

--- contactList/app/controllers/users_controller.rb
+++ contactList/app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
 
   def login
     @user = User.koala(request.env['omniauth.auth']['credentials'])
+    @graph = Koala::Facebook::API.new(oauth_access_token)
+    @profile = @graph.get_object("me")
+    @friends = @graph.get_connections("me", "friends")
   end
 end
