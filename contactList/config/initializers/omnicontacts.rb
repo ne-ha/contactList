4d5fd7c9c639961e86ddab98a6fb9055f6376a23
NEHA SUWAl <neha@jyaasa.com>
2015-03-17 15:29:21 +0545

    Removed importer for facebook.

--- contactList/config/initializers/omnicontacts.rb
+++ contactList/config/initializers/omnicontacts.rb
@@ -4,5 +4,4 @@ Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail,
     "775333370063-v8d4p2rvcbjemhlnjt34u3vm5ekgm88g.apps.googleusercontent.com",
     "xR26XUNhcx6QnGJv8wgl7Zfl", {:redirect_path => "/contacts/gmail/callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt"}
-  importer :facebook, "750035695112072", "91d7cd52089583689650cd2256052bca", {:callback_path => "/contacts/facebook/callback"}
-end
+  end
