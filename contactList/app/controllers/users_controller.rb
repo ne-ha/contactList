f500976110b816c8a28c8f50bb41d86e560fcd8a
NEHA SUWAl <neha@jyaasa.com>
2015-03-17 09:59:24 +0545

    Modified the login method, accessing the friend's list of loged in user

--- contactList/app/controllers/users_controller.rb
+++ contactList/app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  require 'koala'
+
   def index
   end
 
   def login
-    @user = User.koala(request.env['omniauth.auth']['credentials'])
-    @graph = Koala::Facebook::API.new(oauth_access_token)
-    @profile = @graph.get_object("me")
+    @loged_user = User.koala(request.env['omniauth.auth']['credentials'])
+    token = "CAACEdEose0cBAIv6iutkHnCNbkMLMYjCMLW0kks0FYeIXE6Hr0v2dzeJknDdCEZCc7rf1yNfuqCZBZCGCU7dKwJ2PRSa2NnP3cJQMuq9nOQSLCsJ5cxjS20ZAn4epTsm2SiP0vQkRrWQPw5wZBRVbZBMsPSnTe5ZA3ycuekceZCKngA6jrBlybY3ZCjYPqSF83R7O1okU7Mx1eLEI54ZBh8w9F"
+    @graph = Koala::Facebook::API.new(token)
+    @user = @graph.get_object("me")
     @friends = @graph.get_connections("me", "friends")
   end
 end
